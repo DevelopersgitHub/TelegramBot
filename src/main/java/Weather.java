b5cad8c6fedb49401479f60a51d04de726f3eecb
mikhailov.m <mikhailov.miail@yandex.ru>
2019-08-16 23:55:00 +0300

    bot

---
+++ src/main/java/Weather.java
@@ -0,0 +1,42 @@
+import jdk.nashorn.api.scripting.JSObject;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class Weather {
+    public static String getWeather(String message, Model model) throws IOException {
+        URL url = new URL("https://api.openweathermap.org/data/2.5/weather?q=" + message + "&units=metric&appid=ade3258c98927c07348b0a59e41ccff4");
+
+        Scanner in = new Scanner((InputStream) url.getContent());
+        String result = "";
+        while (in.hasNext()) {
+            result += in.nextLine();
+        }
+
+        JSONObject object = new JSONObject(result);
+        model.setName(object.getString("name"));
+
+        JSONObject main = object.getJSONObject("main");
+        model.setTemp(main.getDouble("temp"));
+
+        model.setHumidity(main.getDouble("humidity"));
+
+
+        JSONArray getArray = object.getJSONArray("weather");
+        for (int i = 0; i < getArray.length(); i++) {
+            JSONObject obj = getArray.getJSONObject(i);
+            model.setIcon((String) obj.get("icon"));
+            model.setMain((String) obj.get("main"));
+        }
+
+        return "City: " + model.getName() + "\n" +
+                "Temperature: " + model.getTemp() + "C" + "\n" +
+                "Humidity: " + model.getHumidity() + "%" + "\n" +
+                "http://openweathermap.org/img/w/" + model.getIcon() + ".png";
+    }
+}
